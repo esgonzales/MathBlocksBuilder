2f6410c5eb54c1176dde51ecbafc4ac1c00f7781
esgonzales <40552151+esgonzales@users.noreply.github.com>
2018-07-17 16:50:09 -0400

    Add files via upload

---
+++ MBlock.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace MathBlockBuilder_v2
+{
+    class MBlock
+    {      // MBlock class was written by E.Gonzales for the sole purpose of creating block objects with one int and one image variable. Class is not complete. There are no getters or setters.
+            public int num;
+            public Image block;
+
+            public MBlock()
+            {
+                num = 0;
+                block = Properties.Resources.blk_0;
+            }
+            public MBlock(int numIn, Image blockIn)
+            {
+                num = numIn;
+                block = blockIn;
+            }
+    }
+}
